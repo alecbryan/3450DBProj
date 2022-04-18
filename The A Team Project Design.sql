ca854125c6269317bbbebf12633f81f5f7bdb5c6
alecbryan <92346383+alecbryan@users.noreply.github.com>
2022-04-18 19:11:03 -0400

    Update The A Team Project Design.sql

--- The A Team Project Design.sql
+++ The A Team Project Design.sql
@@ -2,11 +2,11 @@ BEGIN;
 CREATE DATABASE IF NOT EXISTS Fact;
 USE Fact;
 
-CREATE TABLE STORE (
+CREATE TABLE store (
 	STR_ID 		NUMERIC(10,0) PRIMARY KEY
 );
 
-CREATE TABLE PRODUCT (
+CREATE TABLE products (
 	STR_ID		NUMERIC(10,0),
     FOREIGN KEY(STR_ID) REFERENCES STORE(STR_ID),
     ITM_NUM 	NUMERIC(10,0) PRIMARY KEY,
@@ -16,27 +16,28 @@ CREATE TABLE PRODUCT (
     ITM_AMT	NUMERIC(65,0)
 );
 
-CREATE TABLE REMOVED_ITEMS (
+CREATE TABLE product_removed (
 ITM_NUM NUMERIC(10,0),
 FOREIGN KEYS(ITM_NUM) REFERENCES PRODUCT(ITM_NUM)
 ITM_NAME VARCHAR(45),
 ITM_REASON VARCHAR(45)
 );
 
-INSERT INTO products VALUES(1, 'PS5', 2, 799, 3/8/22, 8); 
-INSERT INTO products VALUES(2, 'Xbox Series X', 5, 634, 1/24/22, 15);
-INSERT INTO products VALUES(3, 'Nintendo Switch', 8, 350, 2/4/22, 12);
-INSERT INTO products VALUES(4, 'Super Smash Bros', 21, 45, 2/2/22, 28); 
-INSERT INTO products VALUES(5, 'Call of Duty: Black Ops', 47, 45, 2/24/22, 7); 
-INSERT INTO products VALUES(6, 'Elden Ring', 20, 60, 2/24/22, 30); 
-INSERT INTO products VALUES(7, 'Minecraft', 45, 30, 1/11/22, 20); 
-INSERT INTO products VALUES(8, 'GTA V', 20, 45, 2/13/22, 33);
-INSERT INTO products VALUES(9, 'God of War', 12, 45, 1/3/22, 18); 
-INSERT INTO products VALUES(10, 'Halo 5', 2, 40, 2/8/22, 60); 
-INSERT INTO products VALUES(11, 'Sid Meiers Civilization IV', 14, 25, 12/8/21, 8); 
-INSERT INTO products VALUES(12, 'Turtle Beach Headset', 5, 100, 11/4/21, 4); 
-INSERT INTO products VALUES(13, 'Oculus Quest 2', 5, 400, 3/22/22, 3); 
-INSERT INTO products VALUES(14, 'Xbox Series X Controller', 12, 799, 1/24/22, 8); 
+INSERT INTO store VALUES(1);
+INSERT INTO products VALUES(1, 1, 'PS5', 2, 799, 3/8/22, 8); 
+INSERT INTO products VALUES(1, 2, 'Xbox Series X', 5, 635, 1/24/22, 15);
+INSERT INTO products VALUES(1, 3, 'Nintendo Switch', 8, 350, 2/4/22, 12);
+INSERT INTO products VALUES(1, 4, 'Super Smash Bros', 21, 45, 2/2/22, 28); 
+INSERT INTO products VALUES(1, 5, 'Call of Duty: Black Ops', 47, 45, 2/24/22, 77); 
+INSERT INTO products VALUES(1, 6, 'Elden Ring', 75, 60, 2/24/22, 143); 
+INSERT INTO products VALUES(1, 7, 'Minecraft', 45, 20, 1/11/22, 67); 
+INSERT INTO products VALUES(1, 8, 'GTA V', 20, 45, 2/24/22, 33);
+INSERT INTO products VALUES(1, 9, 'God of War', 12, 45, 2/24/22, 33); 
+INSERT INTO products VALUES(1, 10, 'Halo 5', 2, 40, 2/24/22, 7); 
+INSERT INTO products VALUES(1, 11, 'Sid Meiers Civilization IV', 14, 25, 2/24/22, 46); 
+INSERT INTO products VALUES(1, 12, 'Turtle Beach Headset', 5, 100, 2/28/22, 11); 
+INSERT INTO products VALUES(1, 13, 'Oculus Quest 2', 5, 400, 3/22/22, 13); 
+INSERT INTO products VALUES(1, 14, 'Xbox Series X Controller', 12, 799, 3/8/22, 21); 
 
 
 
